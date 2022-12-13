c5a3dbab861b1fcc8b919fed9e78c682f2f2523c
André Monteiro <andre@dune.com>
2022-12-13 22:23:54 +0000

    Add data to parser

--- dburl_test.go
+++ dburl_test.go
@@ -204,7 +204,7 @@ func TestParse(t *testing.T) {
 		{`tablestore+http://user:pass@localhost/instance_name`, `ots`, `http://user:pass@localhost/instance_name`, ``},
 		{`bend://user:pass@localhost/instance_name?sslmode=disabled&warehouse=wh`, `databend`, `bend://user:pass@localhost/instance_name?sslmode=disabled&warehouse=wh`, ``},
 		{`databend://user:pass@localhost/instance_name?tenant=tn&warehouse=wh`, `databend`, `databend://user:pass@localhost/instance_name?tenant=tn&warehouse=wh`, ``},
-		{`databricks://token:pass@localhost/cluster_id`, `databricks`, `databricks://token:pass@localhost/cluster_id`, ``},
+		{`databricks://token:pass@localhost/cluster_id`, `databricks`, `https://token:pass@localhost:443/cluster_id`, ``},
 	}
 	for i, test := range tests {
 		u, err := Parse(test.s)
